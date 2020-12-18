a0fd75402c44bfd71cf6cb6a930a602a6822b3e6
Marcus Johansson <mj223vn@student.lnu.se>
2020-12-18 16:27:02 +0100

    Copy, paste from google sheet now working and printing the result in the console

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -7,34 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
 
-  headers = ["Value one", "Value two", "Result"];
-
-  rows = [
-    {
-      "Value one" : 543,
-      "Value two" : 75,
-      "Result" : 618
-    },
-    {
-      "Value one" : 345,
-      "Value two" : 65,
-      "Result" : 410
-    },
-    {
-      "Value one" : 215,
-      "Value two" : 175,
-      "Result" : 390
-    },
-    {
-      "Value one" : 823,
-      "Value two" : 83,
-      "Result" : 906
-    },
-  ]
+  
+  headers = ["Value one", "Value two"];
+  
+  rows = [1];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  addData(val: any) {
+    console.log(val)
+  }
 }
