24fcc0c3615ba1ca49e2cc0591776aec9fb57175
Marcus Johansson <mj223vn@student.lnu.se>
2021-01-15 14:51:10 +0100

    manual input working

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -12,15 +12,19 @@ import { HttpClient } from '@angular/common/http';
 export class GridComponent implements OnInit {
 
   url = "http://127.0.0.1:5000/api/post_data"
-  A = [10,11,12,13,14]
-  B = [15,16,17,18,19]
+  A = [10, 11, 12, 13, 14]
+  B = [15, 16, 17, 18, 19]
+
+  numbers = new Array;
+  numCounterA = 0;
+  numCounterB = 0;
 
   groupA = new Array;
   groupB = new Array;
 
 
   headers = ["Group A", "Group B"];
-  
+
   rows = [1];
   postId = 0;
 
@@ -29,35 +33,47 @@ export class GridComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addData(val : any, td_num : number) {
-    if(td_num == 1){
-      this.groupA = convertToNumbers(val);
-    }
-    else{
-      this.groupB = convertToNumbers(val);
-    }
-     
+  addData(val: any, td_num: number) {
+    this.convertToNumbers(val, td_num);
   }
 
-  calculateResults(){
-    console.log(this.groupA);
-    console.log(this.groupB);
-    this.http.post<any>(this.url, {"A":this.A, "B":this.B}).subscribe(data => {
-      this.postId = data.id;
+  onPaste(event: ClipboardEvent, td_num: number) {
+    var data = event.clipboardData
+    var numbers = data?.getData('text');
+    this.convertPasteToNumbers(numbers, td_num)
+  }
+
+  calculateResults() {
+    console.log(this.groupA)
+    console.log(this.groupB)
+    this.http.post<any>(this.url, { "A": this.A, "B": this.B }).subscribe(data => {
+      this.postId = data.id
       console.log(data)
     })
   }
 
-  
-}
+  convertPasteToNumbers(inputData: any, td_num: number) {
+    var numbers = inputData.split("\n")
+    var results = [numbers.length]
+    for (var i = 0; i < numbers.length; i++) {
+      var number = numbers[i].replace(',', '.')
+      results[i] = parseFloat(number)
+    }
+    td_num == 1 ? this.groupA = results : this.groupB = results;
+    console.log(this.groupA)
+    console.log(this.groupB)
+  }
 
-function convertToNumbers(inputData : any) {
-  inputData = inputData.getElementsByTagName('td');
-  var len = inputData.length;
-      var numbersA = [len];
-      for(var i = 0; i < len; i++) {
-        inputData[i].innerHTML =  inputData[i].innerHTML.replace(',', '.');
-        numbersA[i] = parseFloat(inputData[i].innerHTML);
-      }
-  return numbersA;
+  convertToNumbers(inputData: any, td_num: number) {
+    var tdOne = td_num == 1
+    var counter = tdOne ? this.numCounterA : this.numCounterB
+    var number = inputData.innerText.split("\n")
+    console.log(number[0])
+    number = number[counter++].replace(',', '.')
+
+    tdOne ? this.groupA.push(parseFloat(number)) : this.groupB.push(parseFloat(number))
+    tdOne ? this.numCounterA = counter : this.numCounterB = counter
+    console.log(this.groupA)
+    console.log(this.groupB)
+  }
 }
